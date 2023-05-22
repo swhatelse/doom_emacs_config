fe9ce9fe3ae620bdf9465339dde0baa6dace0090
swhatelse <stevenwhatelse@hotmail.fr>
2023-05-22 11:14:50 +0200

    Added files

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yaml-mode py-snippets go-snippets yasnippet-snippets nasm-mode beacon org-bullets flycheck-rust rustic lsp-ui ob-go go-tag flycheck-golangci-lint company-go go-projectile go-mode rust-mode ob-rust)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
